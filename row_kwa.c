0c10249d8111615caf6dc4269c6f61c6dd0cba48
IcemanJT <75624882+IcemanJT@users.noreply.github.com>
2022-10-18 19:55:26 +0200

    Update row_kwa.c

--- row_kwa.c
+++ row_kwa.c
@@ -5,7 +5,7 @@
 int main()
 {
 int a, b, c, delta;
-float pierw, x1, x2, x0;
+float pierw, x1, x2;
 
 printf("podaj a:\n");
 scanf("%d", &a);
@@ -22,7 +22,7 @@ printf("Nie ma miejsc zerowych\n");
 
 
 if (delta == 0){
-x0 = (-b)/(2*a);
+x1 = (-b)/(2*a);
 printf("Miejsce zerowe to: %.2f\n", x0);
 }
 
@@ -30,7 +30,7 @@ if (delta > 0){
 pierw = delta^(1/2);
 x1=((-b)+pierw)/(2*a);
 x2=((-b)-pierw)/(2*a);
-printf("miejsca zerowe to: %.2f i %.2f\n", x1, x2);
+printf("Miejsca zerowe to: %.2f i %.2f\n", x1, x2);
 }
 
 return 0;
