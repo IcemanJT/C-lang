4b00acfd77af2b120dc009d88de8f53bec4c4eb7
IcemanJT <75624882+IcemanJT@users.noreply.github.com>
2022-12-17 13:22:25 +0100

    Update silnia_10.c

--- silnia_10.c
+++ silnia_10.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define IMPROPIET_USAGE 1
+#define WRONG_ARGS 2
+#define RECCURSION 82
+#define ITERATION 73
 
 
 /* rekurencja */
 
-
-int fact(int n){
+int fact_rec(int n){
     if (n>1)
-    return n*fact(n-1);
+    return n*fact_rec(n-1);
     if (n<=1)
         return 1;
 }
@@ -14,8 +19,7 @@ int fact(int n){
 
 /* iteracja */
 
-/*
-int fact(int n){
+int fact_inter(int n){
     int suma = 1;
     for(int i=n; i>0;i--){
         suma*=i;
@@ -23,8 +27,6 @@ int fact(int n){
 
     return suma;
 }
-*/
-
 
 /* 
 int - 13!
@@ -35,10 +37,49 @@ double - do 22! poprawnie, kończy liczenie na 170!
 long double - do 25! poprawnie, kończy liczenie na ok. 1750!
 */
 
-int main(){
-    int x;
-    printf("Podaj x:\n");
-    scanf("%d", &x);
-    printf("%d! = %d\n", x, fact(x));
+int main(int argc, char* argv[])
+{
+    int n, metod;
+    metod = RECCURSION;
+
+    if (argc > 3 || argc < 2){
+        printf("Usage: silnia (int):<n> {-M: R/I}\n");
+        return IMPROPIET_USAGE;
+    }
+    if(argc == 3)
+    {
+        metod = argv[2][0];
+    }
+
+    #ifdef LOG
+    int i;
+
+    for (i=0;i<argc;i++)
+        printf("argv[%d]: %s\n", i, argv[i]);
+
+    printf("argc: %d\n", argc);
+    printf("metod = %d\n", metod);
+    printf("n = %d\n", n);
+    #endif
+
+    n = atoi(argv[1]);
+
+    if (metod == RECCURSION)
+    {
+        printf("%d", fact_rec(n));
+        return 0;
+    }
+    if(metod == ITERATION)
+    {
+        printf("%d", fact_inter(n));
+        return 0;
+    }
+    else
+    {
+        printf("Wrong main args.");
+        return WRONG_ARGS;
+    }
+
+
     return 0; 
 }
